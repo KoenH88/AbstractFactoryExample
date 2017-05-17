912b989abed16f5973094911e3b3d0cf24ba3332
KoenH88 <mail@koenhanselman.nl>
2017-05-17 12:56:11 +0700

    Update AbstractFactoryExample.java

--- AbstractFactoryExample.java
+++ AbstractFactoryExample.java
@@ -2,71 +2,113 @@ package nl.sig.examples;
 
 public class FactoryExample {
 	
-	public class ComponentA
+	public class PizzaStore
 	{
-		public void main(ICardFactory cardfactory) throws Exception{
-			IPaymentcard mastercard = cardfactory.createCard("debit");
-			boolean checkValidity = mastercard.CheckValidity();
+		public void main(IPizzaFactory pizzafatory) throws Exception{
+			IPizza pizza = pizzafactory.createPizza("salami");
+			pizza.bakePizza();
 		}
 	}
 	
-	public interface ICardFactory {
-		IPaymentcard createCard(String cardType) throws Exception;
+	public interface IPizzaFactory {
+		IPaymentcard createPizza(String pizzaType) throws Exception;
 	}
 	
-	public class GeneralFactory implements ICardFactory{
-		public IPaymentcard createCard(String cardtype) throws Exception{
-			switch(cardtype){
-			case "debit":
-				return new DebitCard();
-			case "credit":
-				return new CreditCard();
+	public class ItalianFactory implements IPizzaFactory{
+		public IPizza createPizza(String pizzatype) throws Exception{
+			switch(pizzatype){
+			case "salami":
+				return new PizzaSalami(new ThinCrust());
+			case "tonno":
+				return new PizzaTonno(new ThinCrust());
+			case "quatroformagio":
+				return new PizzaQuatroFormagio(new ThinCrust());
 			default:
-				throw new Exception("Card type not found");
+				throw new Exception("We don't have this pizza, sorry");
 			}
 		}
 	}
 	
-	public interface IPaymentcard
-	{
-		boolean CheckValidity();
-		IPaymentcard requestNewCard();
+	public class AmericanFactory implements IPizzaFactory{
+		public IPizza createPizza(String pizzatype) throws Exception{
+			switch(pizzatype){
+			case "salami":
+				return new PizzaSalami(new ThickCrust());
+			case "tonno":
+				return new PizzaTonno(new ThickCrust());
+			case "quatroformagio":
+				return new PizzaQuatroFormagio(new ThickCrust());
+			default:
+				throw new Exception("We don't have this pizza, sorry");
+			}
+		}
 	}
 	
-	
-
-	public class DebitCard implements IPaymentcard
+	public interface IPizza
 	{
-
-		@Override
-		public boolean CheckValidity() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public IPaymentcard requestNewCard() {
-			// TODO Auto-generated method stub
-			return null;
+		makeDough();
+		bakePizza();
+	}
+	
+	public class PizzaSalami implements IPizza
+	{
+		ICrust crustType;
+		List<Topping> toppings;
+		
+		public PizzaSalami(ICrust crustType)
+		{
+			this.crustType = crustType;
+			base.AddToppings(new List<Topping> { Salami(), new TomatoSaus(), new Cheddar() });
 		}
 		
+		@Override
+		public makeDough() {
+			crustType.GetDough();
+			crustType.RollDough();
+		}
+		
+		@Override
+		public bakePizza() {
+			// Put pizza in oven
+		}	
 	}
 
-	public class CreditCard implements IPaymentcard
+	public class PizzaTonno implements IPizza
 	{
 
 		@Override
-		public boolean CheckValidity() {
-			// TODO Auto-generated method stub
-			return false;
+		public makeDough() {
+			// Make pizza dough
 		}
 
 		@Override
-		public IPaymentcard requestNewCard() {
-			// TODO Auto-generated method stub
-			return null;
+		public addToppings() {
+			// Add tomato sauce, cheese and tuna fish 
+		}
+		
+		@Override
+		public bakePizza() {
+			// Put pizza in oven
+		}	
+	}
+	
+	public class PizzaQuatroFormagio implements IPizza
+	{
+
+		@Override
+		public makeDough() {
+			// Make pizza dough
 		}
 
+		@Override
+		public addToppings() {
+			// Add tomato sauce and 4 cheeses 
+		}
+		
+		@Override
+		public bakePizza() {
+			// Put pizza in oven
+		}	
 	}
 }
 
